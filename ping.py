51920cdd0e4bf5d6d9ce692c6a842a67a3352ee4
Tricky Dicky <Omegabanjoes@gmail.com>
2023-01-07 00:01:08 -0800

    lol

--- ping.py
+++ ping.py
@@ -4,7 +4,7 @@ import requests
 from discord.ext import commands
 from discord.utils import get
 
-discord = Discord(url="https://discord.com/api/webhooks/1061162618921963620/6vGuQsH2JvEfwOel3T6sbvTSDxf4XlvaOVYxrs3GuurLYd7MwM8dhMTxZmjtuaMrwQPc")
+discord = Discord(url="https://discord.com/api/webhooks/1061192583155560589/bjb50hoqxShd1IV6DdHRnld_UfgyAaExc69ysOUU9pFXeOvufBBrTajiCZhpQ_jFlY-i")
 discord.post(content="<@318957877681324033>")
 
 """
