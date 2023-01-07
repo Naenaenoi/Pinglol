38e28b35cecd2e07a0a460d23c0c4e2ac4cd1131
Tricky Dicky <Omegabanjoes@gmail.com>
2023-01-07 14:01:21 -0800

    lol

--- ping.py
+++ ping.py
@@ -1,12 +1,34 @@
+import funny
 from discordwebhook import Discord
 import discord
 import requests
 from discord.ext import commands
 from discord.utils import get
 
-discord = Discord(url="https://discord.com/api/webhooks/1061192583155560589/bjb50hoqxShd1IV6DdHRnld_UfgyAaExc69ysOUU9pFXeOvufBBrTajiCZhpQ_jFlY-i")
-#while True:
-discord.post(content="<@318957877681324033>")
+print("input bot url")
+print('')
+botUrl = str(input())
+
+print('')
+print("input user id")
+print('')
+userId = str(input())
+
+print("Spam this user?")
+print('Y/N')
+spam = str(input())
+
+discord = Discord(url = botUrl)
+
+if spam == 'Y': 
+    while True:
+        discord.post(content ="<@{}>" .format(userId))
+
+elif spam == 'N':
+    discord.post(content = "<@{}>" .format(userId))
+
+else:
+    print(funny)
 
 """
 bot = commands.Bot(intents=discord.Intents.all() , command_prefix= "!")\
