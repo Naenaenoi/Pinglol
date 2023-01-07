cd124fda3fc7c1af60b5843e2a4c9714ff8cce7b
Tricky Dicky <Omegabanjoes@gmail.com>
2023-01-07 00:02:21 -0800

    teehee

--- ping.py
+++ ping.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 from discord.utils import get
 
 discord = Discord(url="https://discord.com/api/webhooks/1061192583155560589/bjb50hoqxShd1IV6DdHRnld_UfgyAaExc69ysOUU9pFXeOvufBBrTajiCZhpQ_jFlY-i")
+#while True:
 discord.post(content="<@318957877681324033>")
 
 """
